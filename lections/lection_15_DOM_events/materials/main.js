ad92e3f8706ffb9136e7e392095c6628d82cc52d
ValeriyTernavskiy <valeraternavckiy1994@gmail.com>
2020-04-22 21:12:46 +0300

    l-15

--- lections/lection_15_DOM_events/materials/main.js
+++ lections/lection_15_DOM_events/materials/main.js
@@ -38,27 +38,97 @@ function handler() {
 // };
 
 
-box.onmouseover = function() {
-    console.log('onmouseover', this);
-    this.classList.add('over');
-};
+// box.onmouseover = function(event) {
+//     console.log('onmouseover', this);
+//     this.classList.add('over');
+// };
 
-box.onmouseout = function() {
-    console.log('onmouseout', this);
-    this.classList.remove('over');
-};
+// box.onmouseout = function() {
+//     console.log('onmouseout', this);
+//     this.classList.remove('over');
+// };
 
-box.oncontextmenu = function() {
-    console.log('contextmenu', this);
-};
+// box.oncontextmenu = function(){
+//     console.log('contextmenu', this);
+// };
 
+// function handlerFirst() {
+//     console.log('to do something handlerFirst');
+// }
+
+// function handlerSecond() {
+//     console.log('to do something handlerSecond');
+// }
+
+// box.onclick = handlerFirst;
+// box.onclick = handlerSecond;
+
+// box.addEventListener('click', handlerFirst);
+// box.addEventListener('click', handlerSecond);
 
 /*
 Coords
 */
 
+// function handlerFirst(event) {
+//     console.log(event.type);
+//     console.log(event.clientX, 'clientX');
+//     console.log(event.clientY, 'clientY');
 
+//     console.log(event.pageX, 'pageX');
+//     console.log(event.pageY, 'pageY');
 
+//     console.log(event.offsetX, 'offsetX');
+//     console.log(event.offsetY, 'offsetY');
+
+// }
+
+    var boxes = document.querySelectorAll('.box');
+
+    function initDragAndDrop(box) {
+        var state = {
+            shiftX: 0,
+            shiftY: 0
+        };
+
+        var draggable = {
+            onDrag: function(event) {
+                console.log('drag START');
+                state.shiftX = event.offsetX;
+                state.shiftY = event.offsetY;
+                document.addEventListener('mousemove', this.onMove);
+            },
+            onMove: function(event) {
+                var x = event.clientX - state.shiftX;
+                var y = event.clientY - state.shiftY;
+                box.style.backgroundColor = getRandomColor();
+                box.style.top = y + 'px';
+                box.style.left = x + 'px';
+            },
+            onDrop: function() {
+                console.log('drag STOP');
+                document.removeEventListener('mousemove', this.onMove);
+            }
+        };
+
+        box.addEventListener('mousedown', draggable.onDrag.bind(draggable));
+        box.addEventListener('mouseup', draggable.onDrop.bind(draggable) )
+    }
+
+    [].forEach.call(boxes, function(box) {
+        initDragAndDrop(box);
+    });
+
+    function getRandomColor() {
+        return 'rgb(' +  getRandom(0, 256) + ',' + getRandom(0, 256) + ',' + getRandom(0, 256) + ')';
+    }
+
+    function getRandom(min , max) {
+        return Math.ceil(Math.random() * (max - min) + min); 
+    }
 
 }
+
+
+
 
