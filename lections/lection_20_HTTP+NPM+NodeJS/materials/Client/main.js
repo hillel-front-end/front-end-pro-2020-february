2c0513169c7c8b7daba6e7da98aefdae450431ea
ValeriyTernavskiy <valeraternavckiy1994@gmail.com>
2020-05-13 18:07:08 +0300

    l-21

--- lections/lection_20_HTTP+NPM+NodeJS/materials/Client/main.js
+++ lections/lection_20_HTTP+NPM+NodeJS/materials/Client/main.js
@@ -1,16 +1,3 @@
-/* 
- var xhttp = new XMLHttpRequest();
- xhttp.onreadystatechange = func(){} -- вызываеися всякий раз когда изменяеться, readyState
- xhttp.readyState - cодержит состояние объекта XMLHttpRequest. Изменяется от 0 до 4:
-    0: запрос не инициализирован
-    1: установлено соединение с сервером
-    2: запрос получен
-    3: обработка запроса
-    4: запрос завершен и ответ готов
-   xhttp.open("GET", "req_url", true);
-   xhttp.send();
-*/
-
 
 // node package manager
 
--- lections/lection_20_HTTP+NPM+NodeJS/materials/NodeJS/server.js
+++ lections/lection_20_HTTP+NPM+NodeJS/materials/NodeJS/server.js
@@ -17,7 +17,7 @@ app.use(function (req, res, next) {
 	next();
 });
 
-const port = 3002;
+const port = 3003;
 
 // запусть наше nodeJs приложении на порту 3002 по адресу http://localhost:3002/
 /* 
@@ -42,7 +42,7 @@ app.listen(port, function () { // говорим на каком порту за
 
 app.get('/', function (req, res) { // req - обьект запроса, res -- обьект ответа
       console.log('THIS PATH - /');// Вызов обработчика на запрос с path ('/')
-      res.status(200); // вернуть статус запроса 200
+      res.status(404); // вернуть статус запроса 200
       res.send({content: 123}); // вернуть данные туда от куда пришел запрос
 });
 
@@ -64,6 +64,29 @@ app.get('/all-users', function(request, responce) {
   .send(data);
 });
 
+const data = [
+{name:"Valera - 2",  age: 10},
+{name:"Valera - 2",  age: 9},
+{name:"Valera - 2",  age: 30},
+{name:"Valera", age: 20}, 
+{name:"Valera - 2",  age: 15}, 
+{name:"Valera - 2",  age: 12}, 
+{name:"Valera - 3",  age: 22}];
+
+app.get('/user', function(req, res) {
+
+  let response = [];
+
+  if (Object.keys(req.query).length) {
+    const {minAge, maxAge} = req.query;
+    response = data.filter(({age}) => age > minAge && age <= maxAge);
+  } else {
+    response = data;
+  }
+  
+  res
+    .send(response);
+});
 
 /*
 '/user/:id' - при запросе http://localhost:3000/user/123, мы можем получить значение :id 
@@ -71,12 +94,15 @@ app.get('/all-users', function(request, responce) {
  Если бы было '/user/:foo', то req.params.foo == 123
 */
 app.get('/user/:id', function(req, res) {
+  console.log(req.query, 'req.query');
+
   fs.readFile('data.json', 'utf-8', function(err, data) { // читает файл json по относительному адресу
           res
           .status(200)
           .send(data); // data - прочитаный json file который возвращаем как результат работы запроса
   });
 });
+
 
 // Получить список всех пользователей
 function getAllusers(req, res) {  
@@ -101,7 +127,7 @@ app.post('/registration', function (req, res) {
   res.send(req.body);
 });
 
-var data = [{name:"Valera"}, {name:"Valera - 2"}, {name:"Valera - 3"}];
+
 
 //http://localhost:3000/registration (get)
 app.get('/registration', function (req, res) {
