ee79a09428e8826f998b58a83e5dce2f04f0267f
ValeriyTernavskiy <valeraternavckiy1994@gmail.com>
2020-06-02 14:20:28 +0300

    l-12

---
+++ lections/lection_26_Vue-Cli_Vuex/materials/app/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    <span class="title">{{foo}}</span>
+
+    <app-auth />
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import Auth from './components/Auth';
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    appAuth: Auth
+  },
+  data() {
+    return {
+      foo: 1236666
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.title {
+  font-size: 40px;
+  color: red;
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ lections/lection_26_Vue-Cli_Vuex/materials/app/src/components/Auth.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="auth">
+        <button @click="goTo">{{btnName}}</button>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Auth',
+  data() {
+    return {
+      btnName: 'This is Auth'
+    }
+  },
+  methods: {
+      goTo() {
+          console.log('go-to');
+      }
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
